b78c13830eda3a464b2bf65e8146eb29457f4971
Nailton Simoes <nailtonsimoes@gmail.com>
2022-08-16 09:39:32 -0300

    push com projeto inicial do back-end

---
+++ back-end/mapa-mundi/src/main/java/com/api/mapamundi/MapaMundiApplication.java
@@ -0,0 +1,13 @@
+package com.api.mapamundi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaMundiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaMundiApplication.class, args);
+	}
+
+}
